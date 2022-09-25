118647b9cde726c688e6fa422936a4baf628398f
realDragonKing <2146844288@qq.com>
2022-09-25 22:25:06 +0800

    集成了springboot，springMVC，mysql、redis客户端，和部分springcloud组件

--- src/main/java/com/blog/Application.java
+++ src/main/java/com/blog/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class AuthenticServiceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthenticServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
