abf29d44bea2c2cd371c75e3308e51d3e29d5c47
realDragonKing <2146844288@qq.com>
2022-09-26 17:40:48 +0800

    集成了springcloud gateway组件

--- src/main/java/com/blog/controller/TestController.java
+++ src/main/java/com/blog/controller/TestController.java
@@ -9,20 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private final RedisTemplate<String, String> redisTemplate0;
-    private final RedisTemplate<String, String> redisTemplate1;
-
-    public TestController(@Qualifier("redisTemplate0") RedisTemplate<String, String> redisTemplate0,
-                          @Qualifier("redisTemplate1") RedisTemplate<String, String> redisTemplate1){
-        this.redisTemplate0 = redisTemplate0;
-        this.redisTemplate1 = redisTemplate1;
-    }
+//    private final RedisTemplate<String, String> redisTemplate0;
+//    private final RedisTemplate<String, String> redisTemplate1;
+//
+//    public TestController(@Qualifier("redisTemplate0") RedisTemplate<String, String> redisTemplate0,
+//                          @Qualifier("redisTemplate1") RedisTemplate<String, String> redisTemplate1){
+//        this.redisTemplate0 = redisTemplate0;
+//        this.redisTemplate1 = redisTemplate1;
+//    }
 
     @RequestMapping(value = "/auth", method = RequestMethod.GET)
     public String test(){
         long time = System.currentTimeMillis();
-        this.redisTemplate0.opsForValue().set("ddd", "ddd");
-        this.redisTemplate1.opsForValue().set("ddd", "ddd");
         return String.valueOf(System.currentTimeMillis() - time);
     }
 
